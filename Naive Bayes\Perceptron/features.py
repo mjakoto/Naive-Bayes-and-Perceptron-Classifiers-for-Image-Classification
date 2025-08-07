0c329396f1642fe2a60b8d9e306557b061bf48ae
mjakoto <137734718+mjakoto@users.noreply.github.com>
2025-08-07 17:00:35 -0400

    Delete Naive Bayes\Perceptron directory

--- Naive Bayes\Perceptron/features.py
+++
@@ -1,9 +0,0 @@
-def image_to_features(image_lines):
-    features = []
-    for line in image_lines:
-        for char in line:
-            if char == ' ':
-                features.append(0)
-            else:
-                features.append(1)
-    return features
